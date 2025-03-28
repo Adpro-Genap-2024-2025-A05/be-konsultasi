f331dcbaae475c5ad3bd1813821baa494e959f9f
Bastian2312 <bastian.adiputras@gmail.com>
2025-03-28 17:47:35 +0700

    init

---
+++ src/main/java/id/ac/ui/cs/advprog/bekonsultasi/BeKonsultasiApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.bekonsultasi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeKonsultasiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeKonsultasiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/bekonsultasi/BeKonsultasiApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.bekonsultasi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeKonsultasiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
