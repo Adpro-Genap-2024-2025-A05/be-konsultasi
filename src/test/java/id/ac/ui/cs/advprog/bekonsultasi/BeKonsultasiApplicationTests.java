70191439ed4d8813f98ecfe1b2b9501b1f22496f
StevenSetiawan <stevensetiawan1802@gmail.com>
2025-04-10 18:56:24 +0700

    feat: finish ci-cd

--- src/test/java/id/ac/ui/cs/advprog/bekonsultasi/BeKonsultasiApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/bekonsultasi/BeKonsultasiApplicationTests.java
@@ -1,13 +1,20 @@
 package id.ac.ui.cs.advprog.bekonsultasi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class BeKonsultasiApplicationTests {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Test
     void contextLoads() {
+        // Verify that the Spring application context loads successfully
+        assertThat(applicationContext).isNotNull();
     }
-
 }
