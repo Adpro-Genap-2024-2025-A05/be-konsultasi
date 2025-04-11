8a516140739b0b8f4e7ec61b5120b0be8d5ef63b
Bastian2312 <bastian.adiputras@gmail.com>
2025-04-11 17:09:58 +0700

    [GREEN] implement schedule factory

---
+++ src/main/java/id/ac/ui/cs/advprog/bekonsultasi/factory/ScheduleFactoryImpl.java
@@ -0,0 +1,40 @@
+package id.ac.ui.cs.advprog.bekonsultasi.factory;
+
+import id.ac.ui.cs.advprog.bekonsultasi.model.AvailableState;
+import id.ac.ui.cs.advprog.bekonsultasi.model.Schedule;
+import id.ac.ui.cs.advprog.bekonsultasi.model.UnavailableState;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class ScheduleFactoryImpl implements ScheduleFactory {
+
+    @Override
+    public Schedule createAvailableSchedule(UUID caregiverId, String day, String time) {
+        Schedule schedule = Schedule.builder()
+                .id(UUID.randomUUID())
+                .caregiverId(caregiverId)
+                .day(day)
+                .time(time)
+                .status("Available")
+                .build();
+
+        schedule.setState(new AvailableState());
+        return schedule;
+    }
+
+    @Override
+    public Schedule createUnavailableSchedule(UUID caregiverId, String day, String time) {
+        Schedule schedule = Schedule.builder()
+                .id(UUID.randomUUID())
+                .caregiverId(caregiverId)
+                .day(day)
+                .time(time)
+                .status("Unavailable")
+                .build();
+
+        schedule.setState(new UnavailableState());
+        return schedule;
+    }
+}
