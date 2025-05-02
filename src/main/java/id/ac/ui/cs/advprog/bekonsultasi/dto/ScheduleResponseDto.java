9bda111cee31efc55529faddb35224929b127f4d
Bastian2312 <bastian.adiputras@gmail.com>
2025-05-02 19:01:56 +0700

    [GREEN] add implementation for schedule dto

---
+++ src/main/java/id/ac/ui/cs/advprog/bekonsultasi/dto/ScheduleResponseDto.java
@@ -0,0 +1,23 @@
+package id.ac.ui.cs.advprog.bekonsultasi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ScheduleResponseDto {
+    private UUID id;
+    private UUID caregiverId;
+    private DayOfWeek day;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private String status;
+}
