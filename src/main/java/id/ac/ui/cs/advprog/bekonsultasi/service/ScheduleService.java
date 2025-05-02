7f05b6476ee282e30175eb2706716b1471188f60
Bastian2312 <bastian.adiputras@gmail.com>
2025-05-02 19:01:03 +0700

    [GREEN] add implementation for schedule service

---
+++ src/main/java/id/ac/ui/cs/advprog/bekonsultasi/service/ScheduleService.java
@@ -0,0 +1,12 @@
+package id.ac.ui.cs.advprog.bekonsultasi.service;
+
+import id.ac.ui.cs.advprog.bekonsultasi.dto.CreateScheduleDto;
+import id.ac.ui.cs.advprog.bekonsultasi.dto.ScheduleResponseDto;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ScheduleService {
+    ScheduleResponseDto createSchedule(CreateScheduleDto dto, UUID caregiverId);
+    List<ScheduleResponseDto> getCaregiverSchedules(UUID caregiverId);
+}
