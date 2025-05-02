17a48c953cd67cc81497f1081bc2c13822540815
StevenSetiawan <stevensetiawan1802@gmail.com>
2025-05-02 19:49:03 +0700

    [GREEN] Implement Konsultasi Repository

---
+++ src/main/java/id/ac/ui/cs/advprog/bekonsultasi/repository/KonsultasiRepository.java
@@ -0,0 +1,16 @@
+package id.ac.ui.cs.advprog.bekonsultasi.repository;
+
+import id.ac.ui.cs.advprog.bekonsultasi.model.Konsultasi;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface KonsultasiRepository extends JpaRepository<Konsultasi, UUID> {
+    List<Konsultasi> findByPacilianId(UUID pacilianId);
+    List<Konsultasi> findByCaregiverId(UUID caregiverId);
+    List<Konsultasi> findByScheduleId(UUID scheduleId);
+    List<Konsultasi> findByStatusAndCaregiverId(String status, UUID caregiverId);
+}
