02fe453ad6b017a725e07b854248b1608572dfe4
StevenSetiawan <stevensetiawan1802@gmail.com>
2025-05-02 19:56:42 +0700

    [GREEN] Implement Konsultasi Service interface

---
+++ src/main/java/id/ac/ui/cs/advprog/bekonsultasi/service/KonsultasiService.java
@@ -0,0 +1,19 @@
+package id.ac.ui.cs.advprog.bekonsultasi.service;
+
+import id.ac.ui.cs.advprog.bekonsultasi.dto.*;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface KonsultasiService {
+    KonsultasiResponseDto createKonsultasi(CreateKonsultasiDto dto, UUID pacilianId);
+    KonsultasiResponseDto confirmKonsultasi(UUID konsultasiId, UUID caregiverId);
+    KonsultasiResponseDto cancelKonsultasi(UUID konsultasiId, UUID userId, String role);
+    KonsultasiResponseDto completeKonsultasi(UUID konsultasiId, UUID caregiverId);
+    KonsultasiResponseDto rescheduleKonsultasi(UUID konsultasiId, RescheduleKonsultasiDto dto, UUID userId, String role);
+    KonsultasiResponseDto getKonsultasiById(UUID konsultasiId);
+    List<KonsultasiResponseDto> getKonsultasiByPacilianId(UUID pacilianId);
+    List<KonsultasiResponseDto> getKonsultasiByCaregiverId(UUID caregiverId);
+    List<KonsultasiResponseDto> getRequestedKonsultasiByCaregiverId(UUID caregiverId);
+    List<KonsultasiHistoryDto> getKonsultasiHistory(UUID konsultasiId);
+}
