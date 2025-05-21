33b2413203a4f5e5753838a38ab946f3ab9bb8fe
Bastian2312 <bastian.adiputras@gmail.com>
2025-05-21 22:37:07 +0700

    [GREEN] Extend factory to support one-time schedule creation

--- src/main/java/id/ac/ui/cs/advprog/bekonsultasi/service/factory/CaregiverScheduleFactory.java
+++ src/main/java/id/ac/ui/cs/advprog/bekonsultasi/service/factory/CaregiverScheduleFactory.java
@@ -1,8 +1,8 @@
 package id.ac.ui.cs.advprog.bekonsultasi.service.factory;
 
 import id.ac.ui.cs.advprog.bekonsultasi.dto.CreateScheduleDto;
+import id.ac.ui.cs.advprog.bekonsultasi.dto.CreateOneTimeScheduleDto;
 import id.ac.ui.cs.advprog.bekonsultasi.model.Schedule;
-import id.ac.ui.cs.advprog.bekonsultasi.model.ScheduleState.AvailableState;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
@@ -11,15 +11,24 @@ import java.util.UUID;
 public class CaregiverScheduleFactory implements ScheduleFactory {
     @Override
     public Schedule createSchedule(CreateScheduleDto dto, UUID caregiverId) {
-        Schedule schedule = Schedule.builder()
+        return Schedule.builder()
                 .caregiverId(caregiverId)
                 .day(dto.getDay())
                 .startTime(dto.getStartTime())
                 .endTime(dto.getEndTime())
-                .status("AVAILABLE")
+                .oneTime(false)
                 .build();
+    }
 
-        schedule.setState(new AvailableState());
-        return schedule;
+    @Override
+    public Schedule createOneTimeSchedule(CreateOneTimeScheduleDto dto, UUID caregiverId) {
+        return Schedule.builder()
+                .caregiverId(caregiverId)
+                .day(dto.getSpecificDate().getDayOfWeek())
+                .startTime(dto.getStartTime())
+                .endTime(dto.getEndTime())
+                .specificDate(dto.getSpecificDate())
+                .oneTime(true)
+                .build();
     }
 }
