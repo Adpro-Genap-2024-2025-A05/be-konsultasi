d1346f3883ddeed8f95649d18648b1d975fb2df2
Bastian2312 <bastian.adiputras@gmail.com>
2025-05-24 16:22:25 +0700

    [REFACTOR] change endpoint

--- src/main/java/id/ac/ui/cs/advprog/bekonsultasi/controller/KonsultasiController.java
+++ src/main/java/id/ac/ui/cs/advprog/bekonsultasi/controller/KonsultasiController.java
@@ -18,7 +18,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/konsultasi")
+@RequestMapping("/api/konsultasi")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 public class KonsultasiController {
